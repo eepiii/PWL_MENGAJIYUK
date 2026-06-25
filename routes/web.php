aa9b54d49bc3c1b5d4b8afc28a4edad5bdc3bc7a
salma <145180375+slmnrrhm@users.noreply.github.com>
2026-06-25 15:30:47 +0700

    Refactor imports in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController; // <-- TAMBAHKAN INI
+use App\Http\Controllers\ProfileController; 
 use App\Http\Controllers\QuranController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\QuranController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
 
-// Halaman Utama (Landing Page)
 Route::get('/', function () {
     return view('welcome');
 });
