38145819e1a8c43b89504f91ec2d8f427b5458dd
Evi Dwi Anggraeni <251351030@watukancana.ac.id>
2026-06-25 17:11:20 +0700

    Menambahkan validasi form dan memfix bug login

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -27,12 +27,21 @@ class LoginRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            'email' => ['required', 'string', 'email'],
-            'password' => ['required', 'string'],
-        ];
+    return [
+        'email' => ['required', 'string', 'email', 'max:255'],
+        'password' => ['required', 'string', 'min:6'],
+    ];
     }
 
+    public function messages(): array
+{
+    return [
+        'email.required' => 'Email wajib diisi.',
+        'email.email' => 'Format email tidak valid.',
+        'password.required' => 'Password wajib diisi.',
+        'password.min' => 'Password minimal 6 karakter.',
+    ];
+}
     /**
      * Attempt to authenticate the request's credentials.
      *
